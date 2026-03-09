08091f6b783f1afacede062b7effef33d24bcfa4
thecoder539473 <trysten.derryberry@okcps.org>
2026-03-09 09:03:23 -0500

    Import LandingPage component in LandingPage.jsx

--- LandingPage.jsx
+++ LandingPage.jsx
@@ -1,3 +1,4 @@
+import LandingPage from './LandingPage';
 import React, { useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import HealthStatusCard from './HealthStatusCard';
